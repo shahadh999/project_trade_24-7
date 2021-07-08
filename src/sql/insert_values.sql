bb1d93696c67ba89d56e2d5a686ddbd5e11a142b
HemangLunagaria <hemang.lunagaria@gmail.com>
2021-07-08 23:49:23 +1000

    Created DB and cleaned data_integration file. Started work on dashboard

---
+++ src/sql/insert_values.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (name, ticker)
+VALUES 
+	   ('Bitcoin', 'BTC-USD'),
+	   ('Ethereum', 'ETH-USD'),
+	   ('Stellar', 'XLM-USD'),
+	   ('Litecoin', 'LTC-USD'),
+	   ('Dash', 'DASH-USD');
